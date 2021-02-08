4495f39ef364e9f58964b4f9a709a37971fc850e
Ryan Shin <changyeolshin@gmail.com>
2021-02-08 10:49:36 +0900

    Made changes to the user.factory.ts for generating fake vehicle information.

--- dist/factories/user.factory.ts
+++ dist/factories/user.factory.ts
@@ -1,6 +1,8 @@
 import Faker from 'faker';
 import { define } from 'typeorm-seeding';
 import { User } from '../../dist/entities/User';
+import vehicleMakers from '../../utils/vehicle-makers';
+import vehicleTypes from '../../utils/vehicle-types';
 
 define(User, (faker: typeof Faker) => {
   const gender: number = faker.random.number(1);
@@ -11,8 +13,10 @@ define(User, (faker: typeof Faker) => {
   const phoneNumber: string = faker.phone.phoneNumber();
   const birthDate: Date = faker.date.recent();
   const occupation: string = faker.name.jobTitle();
-  const vehicleType: string = faker.vehicle.type();
-  const vehicleModel: string = faker.vehicle.model();
+  const vehicleVIN: string = faker.random.uuid();
+  const vehicleType: string = vehicleTypes[Math.floor(Math.random() * vehicleTypes.length)];
+  const vehicleMaker: string = vehicleMakers[Math.floor(Math.random() * vehicleMakers.length)].name;
+  const vehicleModel: string = faker.random.word();
   const vehicleYear: string = Math.floor((Math.random() * 100) + 1900).toString();
   const createdAt: Date = new Date();
   const updatedAt: Date = new Date();
@@ -25,7 +29,10 @@ define(User, (faker: typeof Faker) => {
   user.phoneNumber = phoneNumber;
   user.birthDate = birthDate;
   user.occupation = occupation;
+  user.vehicleVIN = vehicleVIN;
   user.vehicleType = vehicleType;
+  user.vehicleMaker = vehicleMaker;
+  user.vehicleModel = vehicleModel;
   user.vehicleYear = vehicleYear;
   user.createdAt = createdAt;
   user.updatedAt = updatedAt;
