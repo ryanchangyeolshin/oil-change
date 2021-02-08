451556c128e5b56a548ed1ee93b44e688c508697
Ryan Shin <changyeolshin@gmail.com>
2021-02-09 08:43:22 +0900

    Provided types for connection and userRepo variables.

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,15 +1,15 @@
 import { NowRequest, NowResponse } from '@now/node';
-import { Connection, getRepository } from 'typeorm';
+import { Connection, Repository, getRepository } from 'typeorm';
 import { initializeDatabase } from '../../initializers/database';
 import { User } from "../../dist/entities/User";
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const connection = await initializeDatabase();
-  const userRepo = await getRepository(User);
+  const connection: Connection = await initializeDatabase();
+  const userRepo: Repository<User> = await getRepository(User);
   const offset: number = parseInt(req.query.offset.toString());
   const limit: number = parseInt(req.query.limit.toString());
 
   const users = await userRepo.find({ skip: offset, take: limit});
   await connection.close();
-  return res.json({ users });
+  return res.status(200).json({ users });
 };
