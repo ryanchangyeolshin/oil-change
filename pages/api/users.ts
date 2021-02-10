fae667fec312960f0117b542c12cc23b203dd024
Ryan Shin <changyeolshin@gmail.com>
2021-02-10 13:17:23 +0900

    Added try and catch blocks and removed unused imports.

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from '@now/node';
-import { Connection, Repository, getRepository } from 'typeorm';
+import { Connection, Repository, getRepository, AdvancedConsoleLogger } from 'typeorm';
 import { getOrCreateConnection } from '../../initializers/database';
 import { User } from "../../dist/entities/User";
 
@@ -9,7 +9,12 @@ export default async (req: NowRequest, res: NowResponse) => {
   const offset: number = parseInt(req.query.offset.toString());
   const limit: number = parseInt(req.query.limit.toString());
 
-  const users = await userRepo.find({ skip: offset, take: limit});
-  await connection.close();
-  return res.status(200).json({ users });
+  try {
+    const users = await userRepo.find({ skip: offset, take: limit });
+    await connection.close();
+    return res.status(200).json({ users });
+  } catch (e) {
+    await connection.close();
+    return res.status(400).send(e);
+  }
 };
