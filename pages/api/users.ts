9aed2787324020703ad5be350c3b1fd197b9666e
Ryan Shin <changyeolshin@gmail.com>
2021-02-11 19:33:48 +0900

    Provided the route for POST /api/users/:id.

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,20 +1,55 @@
+import { v4 } from 'uuid';
 import { NowRequest, NowResponse } from '@now/node';
 import { Connection, Repository, getRepository, AdvancedConsoleLogger } from 'typeorm';
 import { getOrCreateConnection } from '../../initializers/database';
 import { User } from "../../dist/entities/User";
 
+interface UserPayload {
+  id?: string,
+  firstName?: string,
+  lastName?: string,
+  birthDate?: Date,
+  occupation?: string,
+  email?: string,
+  password?: string,
+  phoneNumber?: string,
+  vehicleVIN?: string,
+  vehicleType?: string,
+  vehicleMake?: string,
+  vehicleModel?: string,
+  vehicleYear?: string
+};
+
 export default async (req: NowRequest, res: NowResponse) => {
+  const method: string | string[] = req.method;
   const connection: Connection = await getOrCreateConnection();
   const userRepo: Repository<User> = await getRepository(User);
-  const offset: number = parseInt(req.query.offset.toString());
-  const limit: number = parseInt(req.query.limit.toString());
 
-  try {
-    const users = await userRepo.find({ skip: offset, take: limit });
-    await connection.close();
-    return res.status(200).json({ users });
-  } catch (e) {
-    await connection.close();
-    return res.status(400).send(e);
+  switch (method) {
+    case 'GET':
+      try {
+        const offset: number = parseInt(req.query.offset.toString());
+        const limit: number = parseInt(req.query.limit.toString());
+        const users = await userRepo.find({ skip: offset, take: limit });
+        await connection.close();
+        return res.status(200).json({ users });
+      } catch (e) {
+        await connection.close();
+        return res.status(400).send(e);
+      }
+      break;
+    case 'POST':
+      const newUserId: string = v4();
+      const newUserPayload: UserPayload = { id: newUserId, ...req.body };
+      try {
+        const user: User = userRepo.create(newUserPayload);
+        await userRepo.save(user);
+        await connection.close();
+        return res.status(200).json({ result: `User ${user.firstName} ${user.lastName}, ID: ${user.id} has been created!` });
+      } catch (e) {
+        await connection.close();
+        return res.status(400).send(e);
+      }
+    default:
   }
 };
--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,10 +1,10 @@
+import { v4 } from 'uuid';
 import { NowRequest, NowResponse } from '@now/node';
 import { Connection, Repository, getRepository } from 'typeorm';
 import { getOrCreateConnection } from '../../../initializers/database';
 import { User } from "../../../dist/entities/User";
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const id: string | string[] = req.query.id;
   const method: string | string[] = req.method;
   const connection: Connection = await getOrCreateConnection();
   const userRepo: Repository<User> = await getRepository(User);
@@ -12,6 +12,7 @@ export default async (req: NowRequest, res: NowResponse) => {
   switch (method) {
     case 'GET':
       try {
+        const id: string | string[] = req.query.id;
         const users: User = await userRepo.findOne(id as string);
         await connection.close();
         return res.status(200).json({ users });
@@ -19,7 +20,7 @@ export default async (req: NowRequest, res: NowResponse) => {
         await connection.close();
         return res.status(400).send(e);
       }
-      break
+      break;
     default:
       res.setHeader('Allow', ['GET'])
       res.status(405).end(`Method ${method} Not Allowed`)
